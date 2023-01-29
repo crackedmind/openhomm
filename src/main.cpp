dfd08929eb0246657065912cfd5541ad898138c0
crackedmind <digital.stream.of.mind@gmail.com>
2023-01-30 04:37:12 +0500

    Update main.cpp to SDL3 and std modules

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
-//#include <iostream>
-//#include <format>
+#define SDL_MAIN_HANDLED
+#include <SDL3/SDL_main.h>
 
-import <format>;
-import <iostream>;
-import <SDL2/SDL.h>;
+import <SDL3/SDL.h>;
 import openhomm.math;
 import openhomm.application;
-
+import openhomm.core;
+import openhomm.io.stream;
+import openhomm.io.print;
+import std;
 
 #if defined(WIN32) && defined(_MSC_VER)
 #   ifdef __clang__
@@ -26,19 +27,40 @@ extern "C" {
 #endif
 
 int main(int argc, char** argv) {
-    Application app(argc, argv);
+    SDL_SetMainReady();
+
+    SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
 
     if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0)
     {
-        std::cout << std::format("SDL_Init fail {}\n", SDL_GetError());
+        core::error("SDL_Init fail {}\n", SDL_GetError());
         return 1;
     }
+
+    SDL_LogSetOutputFunction([](void* userdata, int category, SDL_LogPriority priority, const char* message) {
+        core::log_level level = [priority]() {
+            switch (priority) {
+            case SDL_LOG_PRIORITY_VERBOSE:  return core::log_level::trace;
+            case SDL_LOG_PRIORITY_DEBUG:    return core::log_level::debug;
+            case SDL_LOG_PRIORITY_INFO:     return core::log_level::info;
+            case SDL_LOG_PRIORITY_WARN:     return core::log_level::warning;
+            case SDL_LOG_PRIORITY_ERROR:    return core::log_level::error;
+            case SDL_LOG_PRIORITY_CRITICAL: return core::log_level::critical;
+            default: return core::log_level::none;
+            }
+        }();
+
+        core::log(core::log_level::info, "{}", message);
+    }, nullptr);
+
 
     SDL_DisplayMode current;
     SDL_GetCurrentDisplayMode(0, &current);
 
-    SDL_Window* window = SDL_CreateWindow("Openhomm", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow("Openhomm", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr, SDL_RENDERER_ACCELERATED);
 
+    SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0xFF, 0xFF);
     if (window == nullptr) {
         return 1;
     }
@@ -53,6 +75,15 @@ int main(int argc, char** argv) {
                 break;
             }
         }
+        //Clear screen
+        SDL_RenderClear(renderer);
+
+        //Render texture to screen
+        //SDL_RenderCopy(renderer, gTexture, NULL, NULL);
+
+        //Update screen
+        SDL_RenderPresent(renderer);
+        SDL_Delay(1);
     }
 
 
